50e49a25b5c67d73efeab1624645195e688417e7
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-05-13 00:41:34 +0200

    A bit formatting

--- src/profiling.py
+++ src/profiling.py
@@ -9,10 +9,10 @@ def message_to_user(emotion: str = "😒") -> str:
 def express(emotion: str = "😒") -> str:
     """
     The function "express" takes an emotion as input and returns a corresponding tone in one sentence.
-    
+
     @param emotion The parameter "emotion" is a string that represents a particular emotion. It has a
     default value of "😒" (a grumpy face emoji) if no value is provided when the function is called.
-    
+
     @return If the input parameter `emotion` is "😒", the function will return the string "grumpy and
     sarcastic tone." If the input parameter is "😊", the function will return the string "positive and
     easy-going tone!".
