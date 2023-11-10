f3da0eef8202dc782bc0369fad00aaea3431e4c1
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-11-10 22:33:38 +0100

    Rewriting the app to form chat interaction

--- src/textgetter.py
+++ src/textgetter.py
@@ -2,26 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_text_from_url(url):
+def get_markdown_from_url(url: str) -> str:
     """
-    The function takes a URL as input, downloads the webpage, extracts the text body using
-    BeautifulSoup, removes paragraph breaks and other whitespace, and returns the cleaned text.
-
-    @param url The URL of the webpage that you want to extract text from.
-
-    @return the text content of a webpage after downloading and parsing the HTML. The text is cleaned by
-    removing paragraph breaks and other whitespace before being returned.
+    The function `get_markdown_from_url` takes a URL as input, downloads the webpage content, parses the
+    HTML to extract the text body, and returns the extracted text as a string.
+    
+    @param url The `url` parameter is a string that represents the URL of a webpage.
+    
+    @return The function `get_markdown_from_url` returns a string.
     """
-    # Download the webpage
-    response = requests.get(url)
-    html = response.content
+    try:
+        # Download the webpage
+        response = requests.get(url)
+        html = response.content
 
-    # Parse the HTML to extract the text body
-    soup = BeautifulSoup(html, "html.parser")
-    text = soup.get_text()
+        # Parse the HTML to extract the text body
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.get_text()
 
-    # Remove paragraph breaks and other whitespace
-    text = text.replace("\n", " ")
-    text = " ".join(text.split())
+        return text
 
-    return text
+    except Exception:
+        return ""
